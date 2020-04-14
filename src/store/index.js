a8c4b5469b5441d79d61c902e609f40ce22f03e2
Tim Dohmen <dohmentim@yahoo.com>
2020-04-14 17:28:41 -0600

    can get questions

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
+
+let api = Axios.create({
+  baseURL: "//localhost:3000/api",
+  timeout: 3000,
+  withCredentials: true
+})
 
 export default new Vuex.Store({
   state: {
@@ -9,6 +16,14 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    async getQuestions({ commit, dispatch }) {
+      try {
+        let res = await api.get('questions')
+        console.log("from getQuestions", res)
+      } catch (error) {
+        console.error(error)
+      }
+    }
   },
   modules: {
   }
